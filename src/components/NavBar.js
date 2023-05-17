7c6ebb32198bf3f6ef334a5106c4f497a5f214c3
NickdevC <nick_coles@msn.com>
2023-05-17 14:44:11 +0000

    Add NavBar component and upload logo and favicon

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import WW_logo from "../assets/WW_logo.png";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={WW_logo} alt="logo" height="100" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
