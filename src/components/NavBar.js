74713997c796b37dc8027bb6d1bba3f12efc34a6
NickdevC <nick_coles@msn.com>
2023-05-17 14:53:50 +0000

    Add Font Awesome script and navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,9 +11,16 @@ const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Log In
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign Up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
