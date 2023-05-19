286d05a96c24533e0b63e0176583f718e4a54d2f
NickdevC <nick_coles@msn.com>
2023-05-19 11:36:27 +0000

    Add catch-all route to handle page not found error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route,Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -9,9 +9,10 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
-          <Route exact path="/login" render={()=> <h1>Log In</h1>} />
-          <Route exact path="/signup" render={()=> <h1>Sign Up</h1>} />
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/login" render={() => <h1>Log In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
