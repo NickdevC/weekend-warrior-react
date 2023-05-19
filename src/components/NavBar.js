8be3e6f7fe150ac214d8ca1e5f5fcfce894870d8
NickdevC <nick_coles@msn.com>
2023-05-19 09:00:49 +0000

    Add NavBar.module.css file and style navbar element

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import WW_logo from "../assets/WW_logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={WW_logo} alt="logo" height="100" />
