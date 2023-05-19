68f3787e5f7a771e545bf94e91de5cc3c79118bf
NickdevC <nick_coles@msn.com>
2023-05-19 08:49:32 +0000

    Add Google Fonts link and create App.module.css files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import WW_logo from "../assets/WW_logo.png";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={WW_logo} alt="logo" height="100" />
