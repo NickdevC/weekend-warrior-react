66dee8ed1f5cff110ebfd4ccdaa5904c520b4c6e
NickdevC <nick_coles@msn.com>
2023-05-19 09:52:29 +0000

    Import BrowserRouter in index.js and add Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
+import {Route,Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home Page</h1>
-        <h1>Log In</h1>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+          <Route exact path="/login" render={()=> <h1>Log In</h1>} />
+        </Switch>
       </Container>
     </div>
   );
