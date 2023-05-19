fa83b616bb93b066cf8056bf7afbb4891b7a3eda
NickdevC <nick_coles@msn.com>
2023-05-19 11:21:10 +0000

    Fix issue with react and react-dom versions when importing BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import {Route,Switch} from "react-router-dom";
+import { Route,Switch } from "react-router-dom";
 
 function App() {
   return (
