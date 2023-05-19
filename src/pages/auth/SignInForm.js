81ec8c12adbae49997e45555b3517510ed2a0f18
NickdevC <nick_coles@msn.com>
2023-05-19 15:49:49 +0000

    Add bootstrap form template syntax for SingInForm

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,15 +15,41 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 function SignInForm() {
-//   Add your component logic here
+  //   Add your component logic here
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
-          {/* Add your form here */}
+          <h1 className={styles.Header}>log in</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </Form.Group>
 
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Log In
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
