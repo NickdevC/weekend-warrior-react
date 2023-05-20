396b49b58be0cadd87798fd9ff167e800a84474f
NickdevC <nick_coles@msn.com>
2023-05-20 16:07:40 +0000

    Add handleMount function to App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import './api/axiosDefaults';
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const CurrentUserContext = createContext();
+export const setCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/login" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider>
+      <div className={styles.App}>
+        <NavBar />
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home Page</h1>} />
+            <Route exact path="/login" render={() => <SignInForm />} />
+            <Route exact path="/signup" render={() => <SignUpForm />} />
+            <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
