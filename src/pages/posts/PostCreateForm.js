b3cb4712f744aef9c6adffdfe3133c80de9063f3
NickdevC <nick_coles@msn.com>
2023-05-24 09:59:57 +0000

    Add PostPage.js and CI boilerplate code

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
 
 import Upload from "../../assets/upload.png";
 
