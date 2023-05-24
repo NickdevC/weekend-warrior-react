ef0677e9d3ce99b895900fe898e6d40791810b76
NickdevC <nick_coles@msn.com>
2023-05-24 10:34:36 +0000

    Add bootstrap Card element to return statement of Post.js

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,8 +1,47 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import Avatar from "../../components/Avatar";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Post = (props) => {
-  return <div>Post placeholder text</div>;
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    favourites_count,
+    favourite_id,
+    title,
+    subheading,
+    location,
+    family,
+    weather,
+    terrain,
+    cost,
+    duration,
+    description,
+    post_image,
+    updated_at,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+        </Media>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Post;
