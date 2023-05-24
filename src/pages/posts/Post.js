036c5129452e20b7c7b6a9b57d7f50fb37d58f6b
NickdevC <nick_coles@msn.com>
2023-05-24 10:20:49 +0000

    Add Post.js and import to PostPage.js

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Post.module.css";
+
+const Post = (props) => {
+  return <div>Post placeholder text</div>;
+};
+
+export default Post;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Post from "./Post";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -32,7 +33,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <Post {...post.results[0]} setPosts={setPost} />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
