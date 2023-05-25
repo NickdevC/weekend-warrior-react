50bc89af00201bd0652f5c2f5c73f0dec6ef3c0c
NickdevC <nick_coles@msn.com>
2023-05-25 16:01:02 +0000

    Add PostsPage.js and starter CI code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/adventures/PostCreateForm";
 import PostPage from "./pages/adventures/PostPage";
+import PostsPage from "./pages/adventures/PostsPage";
 
 function App() {
   return (
@@ -14,10 +15,20 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
+          />
           <Route exact path="/login" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/adventures/create" render={() => <PostCreateForm />} />
+          <Route
+            exact
+            path="/adventures/create"
+            render={() => <PostCreateForm />}
+          />
           <Route exact path="/adventures/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
