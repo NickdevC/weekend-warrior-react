c4d602d34d36611fe5bae8d0237b73d46eae90be
NickdevC <nick_coles@msn.com>
2023-05-27 09:46:42 +0000

    Add MoreDropdown component to the Comment function

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,13 @@ import styles from "../../styles/Comment.module.css";
 import Avatar from "../../components/Avatar";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, response } = props;
+  const currentUser = useCurrentUser()
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div>
@@ -19,6 +23,9 @@ const Comment = (props) => {
           <span className={styles.Date}>{updated_at}</span>
           <p>{response}</p>
         </Media.Body>
+        {is_owner && (
+          <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+        )}
       </Media>
     </div>
   );
