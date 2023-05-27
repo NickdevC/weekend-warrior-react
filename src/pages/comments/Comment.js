ed6b23d166aa8080fc77e7d2c62e11f9835496d4
NickdevC <nick_coles@msn.com>
2023-05-27 09:58:33 +0000

    Add handleDelete function to comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -5,11 +5,39 @@ import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, updated_at, response } = props;
-  const currentUser = useCurrentUser()
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    response,
+    id,
+    setPost,
+    setComments,
+  } = props;
+  const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -24,7 +52,7 @@ const Comment = (props) => {
           <p>{response}</p>
         </Media.Body>
         {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
         )}
       </Media>
     </div>
