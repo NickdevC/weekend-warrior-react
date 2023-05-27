44abe2cd4bad73544e4496c356e2fb3d52df51f2
NickdevC <nick_coles@msn.com>
2023-05-27 09:03:53 +0000

    Map over the comments and display beneath each post

--- src/pages/adventures/PostPage.js
+++ src/pages/adventures/PostPage.js
@@ -21,11 +21,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/adventures/${id}`),
+          axiosReq.get(`/comments/?adventure=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -51,6 +52,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <p key={comment.id}>
+                {comment.owner}: {comment.response}
+              </p>
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments yet...</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
