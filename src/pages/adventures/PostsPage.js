6519f1c96bd80a6f0ed9c128238398e54187e5e5
NickdevC <nick_coles@msn.com>
2023-05-27 10:47:18 +0000

    Add PopularProfiles.js and render in PostsPage.js

--- src/pages/adventures/PostsPage.js
+++ src/pages/adventures/PostsPage.js
@@ -15,6 +15,7 @@ import NoResults from "../../assets/WW-no-results.png";
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -88,7 +89,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
