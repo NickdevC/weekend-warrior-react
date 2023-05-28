50f802f9a2f3a7fbe29d866dd4ca10f601f2d8a5
NickdevC <nick_coles@msn.com>
2023-05-28 11:05:37 +0000

    Add further tests checking links to NavBar.test.js

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
 import NavBar from "../NavBar";
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
 
 test("renders NavBar", () => {
   render(
@@ -13,3 +14,35 @@ test("renders NavBar", () => {
   const signInLink = screen.getByRole("link", { name: "Log In" });
   expect(signInLink).toBeInTheDocument();
 });
+
+test("renders link to the user profile for a logged in user", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  const profileAvatar = await screen.findByText('Profile')
+  expect(profileAvatar).toBeInTheDocument()
+});
+
+test("renders Log In and Sign Up buttons again on log out", async () => {
+    render(
+      <Router>
+        <CurrentUserProvider>
+          <NavBar />
+        </CurrentUserProvider>
+      </Router>
+    );
+  
+    const signOutLink = await screen.findByRole('link', {name: 'Log Out'})
+    fireEvent.click(signOutLink)
+
+    const signInLink = await screen.findByRole('link', {name: 'Log In'})
+    const signUpLink = await screen.findByRole('link', {name: 'Sign Up'})
+
+    expect(signInLink).toBeInTheDocument();
+    expect(signUpLink).toBeInTheDocument();
+  });
